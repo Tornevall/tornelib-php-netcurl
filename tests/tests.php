f4c5cb5d01df4f4f863357e933d8e89c64d2b206
Tomas Tornevall <thorne@tornevall.net>
2017-10-24 09:53:46 +0200

    LIB-136: netcurl - clearAllFlags() LIB-139: Save debugging information of each call made LIB-140: Automatically detect if a website protocol is http or https LIB-141: _DEBUG_TCURL_UNSET_LOCAL_PEM_LOCATION should be flags, not defines LIB-142: Move crypto tests to crypto module

--- tests/tests.php
+++ tests/tests.php
@@ -16,20 +16,10 @@ class Tornevall_cURLTest extends TestCase {
 	/** @var Tornevall_cURL */
 	private $CURL;
 	/** @var TorneLIB_Crypto */
-	private $Crypto;
 	private $Urls;
 	private $TorSetupAddress = "127.0.0.1:9050";
 	private $TorSetupType = 4;      /* CURLPROXY_SOCKS4*/
 	private $CurlVersion = null;
-
-	// Compressed strings setup over base64
-	private $testCompressString = "Testing my string";
-	private $gz0Base = "H4sIAAAAAAAEAwERAO7_VGVzdGluZyBteSBzdHJpbmf030_XEQAAAA";
-	private $gz9Base = "H4sIAAAAAAACAwtJLS7JzEtXyK1UKC4pArIA9N9P1xEAAAA";
-	private $bzBase = "QlpoNDFBWSZTWajSZJAAAAETgEAABAACoxwgIAAhoaA0IBppoKc4F16DpQXi7kinChIVGkySAA";
-
-	private $testLongCompressString = "The following string contains data: This is a longer string to test the best compression on something that is worth compression.";
-	private $testLongCompressedString = "H4sIAAAAAAACA02MQQrDMAwEv6IX5N68Ix9QU9UyONpgLQTy-tqFQmFg9zBMuR_r5iZvtIarRpFkn7MjqDVSXkpdZfOaMlBpiGL9pxFCSwpH4znPjuPsllkRMkgcRv-arpyFC53-ry0flwqd0IQAAAA";
 
 	public $specUrlUsername;
 	public $specUrlPassword;
@@ -40,8 +30,6 @@ class Tornevall_cURLTest extends TestCase {
 		$this->StartErrorReporting = error_reporting();
 		$this->NET                 = new \TorneLIB\TorneLIB_Network();
 		$this->CURL                = new \TorneLIB\Tornevall_cURL();
-		$this->Crypto              = new \TorneLIB\TorneLIB_Crypto();
-
 		$this->CURL->setUserAgent("TorneLIB/NetCurl-PHPUNIT");
 
 		if ( function_exists( 'curl_version' ) ) {
@@ -68,9 +56,14 @@ class Tornevall_cURLTest extends TestCase {
 		);
 	}
 
-	public function __construct( $name = null, array $data = [], $dataName = '' ) {
-		parent::__construct( $name, $data, $dataName );
+	function tearDown() {
+		// DebugData collects stats about the curled session.
+		// $debugData = $this->CURL->getDebugData();
 	}
+
+	/*public function __construct( $name = null, array $data = [], $dataName = '' ) {
+		parent::__construct( $name, $data, $dataName );
+	}*/
 
 	private function pemDefault() {
 		$this->CURL->setFlag('_DEBUG_TCURL_UNSET_LOCAL_PEM_LOCATION', false);
@@ -147,45 +140,6 @@ class Tornevall_cURLTest extends TestCase {
 		}
 	}
 	*/
-
-	function testBase64GzEncodeLevel0() {
-		$gzString = $this->Crypto->base64_gzencode( $this->testCompressString, 0 );
-		$this->assertTrue( $gzString == $this->gz0Base );
-	}
-
-	function testBase64GzEncodeLevel9() {
-		$myString = "Testing my string";
-		$gzString = $this->Crypto->base64_gzencode( $myString, 9 );
-		$this->assertTrue( $gzString == $this->gz9Base );
-	}
-
-	function testBase64GzDecodeLevel0() {
-		$gzString = $this->Crypto->base64_gzdecode( $this->gz0Base );
-		$this->assertTrue( $gzString == $this->testCompressString );
-	}
-
-	function testBase64GzDecodeLevel9() {
-		$gzString = $this->Crypto->base64_gzdecode( $this->gz9Base );
-		$this->assertTrue( $gzString == $this->testCompressString );
-	}
-
-	function testBase64BzEncode() {
-		$bzString = $this->Crypto->base64_bzencode( $this->testCompressString );
-		$this->assertTrue( $bzString == $this->bzBase );
-	}
-
-	function testBase64BzDecode() {
-		$bzString = $this->Crypto->base64_bzdecode( $this->bzBase );
-		$this->assertTrue( $bzString == $this->testCompressString );
-	}
-
-	function testBestCompression() {
-		$compressedString                  = $this->Crypto->base64_compress( $this->testLongCompressString );
-		$uncompressedString                = $this->Crypto->base64_decompress( $compressedString );
-		$uncompressedStringCompressionType = $this->Crypto->base64_decompress( $compressedString, true );
-		// In this case the compression type has really nothing to do with the test. We just know that gz9 is the best type for our chosen data string.
-		$this->assertTrue( $uncompressedString == $this->testLongCompressString && $uncompressedStringCompressionType == "gz9" );
-	}
 
 	/**
 	 * Runs a simple test to see if there is a container as it should

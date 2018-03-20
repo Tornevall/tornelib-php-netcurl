bc6ed762b364f63506a89aeaf114709500fbb417
Tomas Tornevall <thorne@tornevall.net>
2018-03-20 15:26:42 +0100

    NetCurl Merger for deprecated systems

--- merge.sh
+++ merge.sh
@@ -3,3 +3,18 @@
 # Compatibility generator for NetCurl v1.0
 # Remove content after namespace.
 # sed -e '1,/namespace/d' module_*.php >network.php
+
+src=`grep source composer.json | sed 's/[\"|,$]//g'`
+mergeTo="source/netcurl.php"
+
+for mergeFile in $src
+do
+    if [ ! $firstFile ] ; then
+        firstFile=1
+        echo "Initializing merge with ${mergeFile}"
+        cat $mergeFile >${mergeTo}
+    else
+        echo "Merging ${mergeFile} into ${mergeTo}"
+        sed -e '1,/namespace/d' $mergeFile >>${mergeTo}
+    fi
+done

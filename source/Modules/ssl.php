393fef6e862e7b47933e7792c97f36b1752d481e
Tomas Tornevall <thorne@tornevall.net>
2018-03-25 08:43:37 +0200

    LIB-194: Migrate SSL controls into its own module LIB-198: CURL/SSL: Migrate sslGetOptionsStream LIB-195: CURL/SSL: Migrate sslStreamContextCorrection LIB-196: CURL/SSL: Migrate openssl_guess()  [getSslCertificateBundle]

--- source/Modules/ssl.php
+++ source/Modules/ssl.php
@@ -18,7 +18,7 @@
  * Tornevall Networks netCurl library - Yet another http- and network communicator library
  * Each class in this library has its own version numbering to keep track of where the changes are. However, there is a major version too.
  * @package TorneLIB
- * @version 6.0.6
+ * @version 6.0.0
  */
 
 namespace TorneLIB;
@@ -27,11 +27,7 @@ namespace TorneLIB;
  * Class MODULE_SSL SSL Helper class
  * @package TorneLIB
  */
-abstract class MODULE_SSL {
-	//// SSL AUTODETECTION CAPABILITIES
-	/// DEFAULT: Most of the settings are set to be disabled, so that the system handles this automatically with defaults
-	/// If there are problems reaching wsdl or connecting to https-based URLs, try set $testssl to true
-
+class MODULE_SSL {
 	/**
 	 * @var bool $testssl
 	 *
@@ -42,14 +38,41 @@ abstract class MODULE_SSL {
 	 */
 	private $testssl = true;
 	/** @var bool Do not test certificates on older PHP-version (< 5.6.0) if this is false */
-	private $testssldeprecated = false;
-	/** @var bool If there are problems with certificates bound to a host/peer, set this to false if necessary. Default is to always try to verify them */
-	private $sslVerify = true;
-	/** @var array Error messages from SSL loading */
 	private $sslDriverError = array();
 	/** @var bool If SSL has been compiled in CURL, this will transform to true */
 	private $sslCurlDriver = false;
 
+	/** @var array Default paths to the certificates we are looking for */
+	private $sslPemLocations = array( '/etc/ssl/certs' );
+	/** @var array Files to look for in sslPemLocations */
+	private $sslPemFiles = array( 'cacert.pem', 'ca-certificates.crt' );
+	/** @var string Location of the SSL certificate bundle */
+	private $sslRealCertLocation;
+	/** @var bool Strict verification of the connection (sslVerify) */
+	private $SSL_STRICT_VERIFICATION = true;
+	/** @var null|bool Allow self signed certificates */
+	private $SSL_STRICT_SELF = null;
+	/** @var bool Allowing fallback to unstict verification */
+	private $SSL_UNSTRICT_FALLBACK = false;
+
+	/** @var MODULE_CURL $PARENT */
+	private $PARENT;
+
+	/**
+	 * MODULE_SSL constructor.
+	 *
+	 * @param MODULE_CURL $MODULE_CURL
+	 */
+	function __construct( $MODULE_CURL = null ) {
+		if ( is_object( $MODULE_CURL ) ) {
+			$this->PARENT = $MODULE_CURL;
+		}
+	}
+
+	/**
+	 * @return array
+	 * @since 6.0.0
+	 */
 	public static function getCurlSslAvailable() {
 		// Common ssl checkers (if they fail, there is a sslDriverError to recall
 
@@ -63,7 +86,7 @@ abstract class MODULE_SSL {
 
 		if ( function_exists( 'curl_version' ) ) {
 			$curlVersionRequest = curl_version();
-			$curlVersion  = $curlVersionRequest['version'];
+			$curlVersion        = $curlVersionRequest['version'];
 			if ( defined( 'CURL_VERSION_SSL' ) ) {
 				if ( isset( $curlVersionRequest['features'] ) ) {
 					$CURL_SSL_AVAILABLE = ( $curlVersionRequest['features'] & CURL_VERSION_SSL ? true : false );
@@ -75,6 +98,154 @@ abstract class MODULE_SSL {
 				}
 			}
 		}
+
 		return $sslDriverError;
+	}
+
+	/**
+	 * openssl_guess rewrite
+	 *
+	 * @return string
+	 * @since 6.0.0
+	 */
+	public function getSslCertificateBundle() {
+		// Assume that sysadmins can handle this, if open_basedir is set as things will fail if we proceed here
+		if ( ini_get( 'open_basedir' ) != '' ) {
+			return '';
+		}
+
+		foreach ( $this->sslPemLocations as $filePath ) {
+			if ( is_dir( $filePath ) && ! in_array( $filePath, $this->sslPemLocations ) ) {
+				$this->sslPemLocations[] = $filePath;
+			}
+		}
+
+		// If PHP >= 5.6.0, the OpenSSL module has its own way getting certificate locations
+		if ( version_compare( PHP_VERSION, "5.6.0", ">=" ) && function_exists( "openssl_get_cert_locations" ) ) {
+			$internalCheck = openssl_get_cert_locations();
+			if ( isset( $internalCheck['default_cert_dir'] ) && is_dir( $internalCheck['default_cert_dir'] ) && ! empty( $internalCheck['default_cert_file'] ) ) {
+				$certFile = basename( $internalCheck['default_cert_file'] );
+				if ( ! in_array( $internalCheck['default_cert_dir'], $this->sslPemLocations ) ) {
+					$this->sslPemLocations[] = $internalCheck['default_cert_dir'];
+				}
+				if ( ! in_array( $certFile, $this->sslPemFiles ) ) {
+					$this->sslPemFiles[] = $certFile;
+				}
+			}
+		}
+
+		// get first match
+		foreach ( $this->sslPemLocations as $location ) {
+			foreach ( $this->sslPemFiles as $file ) {
+				$fullCertPath = $location . "/" . $file;
+				if ( file_exists( $fullCertPath ) && empty( $this->sslRealCertLocation ) ) {
+					$this->sslRealCertLocation = $fullCertPath;
+				}
+			}
+		}
+
+		return $this->sslRealCertLocation;
+	}
+
+	/**
+	 * @param array $pemLocationData
+	 *
+	 * @since 6.0.20
+	 */
+	public function setPemLocation( $pemLocationData = array() ) {
+
+	}
+
+	public function getPemLocations() {
+		return $this->sslPemLocations;
+	}
+
+	/**
+	 * @param bool $strictifyAllVerifications
+	 * @param bool $strictifySelfSigned
+	 *
+	 * @since 6.0.0
+	 */
+	public function setStrictVerification( $strictifyAllVerifications = true, $strictifySelfSigned = true ) {
+		$this->SSL_STRICT_VERIFICATION = $strictifyAllVerifications;
+		$this->SSL_STRICT_SELF         = $strictifySelfSigned;
+	}
+
+	/**
+	 * Returns the mode of strict verification set up. If true, netcurl will be very strict with all certificate verifications.
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public function getStrictVerification() {
+		return $this->SSL_STRICT_VERIFICATION;
+	}
+
+	/**
+	 * Replacement for allowSslUnverified setup
+	 *
+	 * @param bool $unstrictifyVerification *
+	 *
+	 * @since 6.0.0
+	 */
+	public function setSslUnstrictFallback( $unstrictifyVerification = false ) {
+		$this->SSL_UNSTRICT_FALLBACK = $unstrictifyVerification;
+	}
+
+	/**
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public function getSslUnstrictFallback() {
+		return $this->SSL_UNSTRICT_FALLBACK;
+	}
+
+	/**
+	 * Prepare context stream for SSL
+	 *
+	 * @return array
+	 *
+	 * @since 6.0.0
+	 */
+	public function getSslStreamContext() {
+		return array(
+			'cafile'            => $this->getSslCertificateBundle(),
+			'verify_peer'       => $this->SSL_STRICT_VERIFICATION,
+			'verify_peer_name'  => $this->SSL_STRICT_VERIFICATION,
+			'verify_host'       => $this->SSL_STRICT_VERIFICATION,
+			'allow_self_signed' => $this->SSL_STRICT_SELF
+		);
+	}
+
+	/**
+	 * Put the context into stream for SSL
+	 *
+	 * @param array $optionsArray
+	 * @param array $addonContextData
+	 *
+	 * @return array
+	 * @since 6.0.0
+	 */
+	public function getSslStream( $optionsArray = array(), $addonContextData = array() ) {
+		$streamContextOptions = array();
+		if ( is_object( $this->PARENT ) ) {
+			$streamContextOptions['http'] = array(
+				"user_agent" => $this->PARENT->getUserAgent()
+			);
+		}
+		$sslCorrection = $this->getSslStreamContext();
+		if ( count( $sslCorrection ) ) {
+			$streamContextOptions['ssl'] = $this->getSslStreamContext();
+		}
+		if ( is_array( $addonContextData ) && count( $addonContextData ) ) {
+			foreach ( $addonContextData as $contextKey => $contextValue ) {
+				$streamContextOptions[ $contextKey ] = $contextValue;
+			}
+		}
+		$optionsArray['stream_context'] = stream_context_create( $streamContextOptions );
+		$this->sslopt                   = $optionsArray;
+
+		return $optionsArray;
+
 	}
 }

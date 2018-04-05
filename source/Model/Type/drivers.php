5878edd1eb84f773e5310c2fbf5b2cfe2c100e5d
Tomas Tornevall <thorne@tornevall.net>
2018-04-05 12:28:29 +0200

    LIB-193: "Internal" changed to "curl" as internals uses curl by default

--- source/Model/Type/drivers.php
+++ source/Model/Type/drivers.php
@@ -30,10 +30,15 @@ if ( ! class_exists( 'NETCURL_NETWORK_DRIVERS' ) && ! class_exists( 'TorneLIB\NE
 	 */
 	abstract class NETCURL_NETWORK_DRIVERS {
 		const DRIVER_NOT_SET = 0;
-		const DRIVER_INTERNAL = 1;
+		const DRIVER_CURL = 1;
 		const DRIVER_WORDPRESS = 1000;
 		const DRIVER_GUZZLEHTTP = 1001;
 		const DRIVER_GUZZLEHTTP_STREAM = 1002;
+
+		/**
+		 * @deprecated Internal driver should be named DRIVER_CURL
+		 */
+		const DRIVER_INTERNAL = 1;
 	}
 }
 if ( ! class_exists( 'TORNELIB_CURL_DRIVERS' ) && ! class_exists( 'TorneLIB\TORNELIB_CURL_DRIVERS' ) ) {

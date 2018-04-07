08c41811eb6501f472a4bda2f4c1f1e589c274ec
Tomas Tornevall <thorne@tornevall.net>
2018-04-07 08:22:26 +0200

    LIB-243: Extend NETCURL_CURLOBJECT by deprecating the old class, replace with NETCURL_HTTP_OBJECT

--- source/Model/Type/curlobject.php
+++ source/Model/Type/curlobject.php
@@ -22,28 +22,69 @@
 
 namespace TorneLIB;
 
-if ( ! class_exists( 'NETCURL_CURLOBJECT' ) && ! class_exists( 'TorneLIB\NETCURL_CURLOBJECT' ) ) {
+if ( ! class_exists( 'NETCURL_HTTP_OBJECT' ) && ! class_exists( 'TorneLIB\NETCURL_HTTP_OBJECT' ) ) {
 	/**
 	 * Class NETCURL_CURLOBJECT
 	 * @package TorneLIB
 	 * @since 6.0.20
 	 * @todo Getters and setters
 	 */
-	class NETCURL_CURLOBJECT {
-		public $header;
-		public $body;
-		public $code;
-		public $parsed;
-		public $url;
-		public $ip;
+	class NETCURL_HTTP_OBJECT {
+
+		private $NETCURL_HEADER;
+		private $NETCURL_BODY;
+		private $NETCURL_CODE;
+		private $NETCURL_PARSED;
+		private $NETCURL_URL;
+		private $NETCURL_IP;
+
+		public function __construct($header = array(), $body = '', $code = 0, $parsed = '', $url = '', $ip = '') {
+			$this->NETCURL_HEADER = $header;
+			$this->NETCURL_BODY = $body;
+			$this->NETCURL_CODE = $code;
+			$this->NETCURL_PARSED = $parsed;
+			$this->NETCURL_URL = $url;
+			$this->NETCURL_IP = $ip;
+		}
+
+		public function getHeader() {
+			return $this->NETCURL_HEADER;
+		}
+
+		public function getBody() {
+			return $this->NETCURL_BODY;
+		}
+
+		public function getCode() {
+			return $this->NETCURL_CODE;
+		}
+
+		public function getParsed() {
+			return $this->NETCURL_PARSED;
+		}
+
+		public function getUrl() {
+			$this->NETCURL_URL;
+		}
+
+		public function getIp() {
+			return $this->NETCURL_IP;
+		}
+
 	}
 }
 if ( ! class_exists( 'TORNELIB_CURLOBJECT' ) && ! class_exists( 'TorneLIB\TORNELIB_CURLOBJECT' ) ) {
 	/**
 	 * Class TORNELIB_CURLOBJECT
 	 * @package TorneLIB
-	 * @deprecated Use NETCURL_CURLOBJECT
+	 * @deprecated 6.0.20 Use NETCURL_HTTP_OBJECT
 	 */
-	class TORNELIB_CURLOBJECT extends NETCURL_CURLOBJECT {
+	class TORNELIB_CURLOBJECT extends NETCURL_HTTP_OBJECT {
+		public $header;
+		public $body;
+		public $code;
+		public $parsed;
+		public $url;
+		public $ip;
 	}
 }

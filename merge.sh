08dbe1e75c2334040c61be26969cbb04814ae2dd
Tomas Tornevall <thorne@tornevall.net>
2018-04-25 08:51:11 +0200

    Merger patched and version update in head-doc

--- merge.sh
+++ merge.sh
@@ -7,6 +7,8 @@
 src=`grep source\/ composer.json | sed 's/[\"|,$]//g'`
 mergeTo="source/build/netcurl.php"
 
+namespace="TorneLIB"
+
 for mergeFile in $src
 do
     if [ ! $firstFile ] ; then
@@ -25,4 +27,11 @@ io=`find |grep tornevall_io.php`
 if [ "" != "$io" ] ; then
     echo "Merging ${io} into ${mergeTo}"
     sed -e '1,/namespace/d' $io >>${mergeTo}
+fi
+
+if [ "" != "$1" ] ; then
+    # Usage:
+    # ./merge.sh "Resursbank\\\RBEcomPHP"
+    echo "Update merged library with new namespace: $namespace => $1"
+    sed -i "s/namespace TorneLIB/namespace $1/" ${mergeTo}
 fi

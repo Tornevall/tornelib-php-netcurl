334932f912c8e33518a75e4dfc4c024f76741c84
Tomas Tornevall <thorne@tornevall.net>
2018-07-27 20:25:30 +0200

    NETCURL-212: Additional tests for imap added

---
+++ tests/additionalTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TorneLIB;
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
+}
+
+use PHPUnit\Framework\TestCase;
+use TorneLIB\MODULE_CURL;
+
+/**
+ * Class additionalTest Class for testing additional protocols (currently: imap)
+ *
+ * @package TorneLIB
+ */
+class additionalTest extends TestCase
+{
+    /** @var MODULE_CURL */
+    private $CURL;
+
+    /** @var string Change this */
+    private $ACCOUNT = "imap://user:password@imap.server";
+
+    function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+
+        $this->CURL = new MODULE_CURL();
+    }
+
+    /**
+     * @test
+     */
+    function testImap() {
+        if (preg_match("/user\:password/", $this->ACCOUNT)) {
+            static::markTestSkipped("Testing imap connectivity requires proper user login data");
+            return;
+        }
+        try {
+            $imapRequest = $this->CURL->doGet($this->ACCOUNT)->getHeader();
+            static::assertTrue((bool)preg_match("/LIST(.*?)INBOX/", $imapRequest));
+        } catch (\Exception $e) {
+            static::markTestSkipped($e->getMessage());
+        }
+    }
+}

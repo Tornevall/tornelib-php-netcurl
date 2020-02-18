a0741b02c8aa54aad15068924a3e051aeb567027
Tomas Tornevall <thorne@tornevall.net>
2020-02-18 19:03:22 +0100

    NETCURL-243: SSL support
    Readded streamcontext generator in the MODULE. Also added deprecation docblocks in 6.0 for which methods that are changing or goes removed in 6.1.

--- tests/sslWrapperTest.php
+++ tests/sslWrapperTest.php
@@ -1,8 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use TorneLIB\Helpers\SSL;
 use TorneLIB\Flags;
+use TorneLIB\Helpers\SSL;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
@@ -33,7 +33,43 @@ class sslWrapperTest extends TestCase
      */
     public function sslWrappers()
     {
-        $SSL = new SSL();
-        static::assertTrue($SSL->getSslCapabilities());
+        static::assertTrue((new SSL())->getSslCapabilities());
+    }
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function strictValidation()
+    {
+        $sslAction = new SSL();
+        $sslAction->setStrictVerification(false);
+        $verifyPeerChange = $sslAction->getSecurityLevelChanges();
+
+        /*
+         * Tests includes:
+         *  - Disable verify_peer (assert false).
+         *  - Check the default content of allow_self_signed (assert).
+         *  - Set own context and validates content.
+         *  - Set own context and validates content by keyed call to getContext()
+         *  - Verify that security level changes are "logged".
+         */
+        static::assertTrue(
+            !(bool)$sslAction->getContext()['verify_peer'] &&
+            (bool)!(new SSL())->getContext()['allow_self_signed'] &&
+            (new SSL())->setContext('passphrase', 'simple_phrase')->getContext()['passphrase'] &&
+            (new SSL())->setContext('passphrase', 'simple_phrase')->getContext('passphrase') === 'simple_phrase' &&
+            is_array($verifyPeerChange) &&
+            count($verifyPeerChange) === 1
+        );
+    }
+
+    /**
+     * @test
+     * @testdox Make sure the streamcontext are created properly.
+     */
+    public function getPreparedSslContext()
+    {
+        static::assertTrue(is_resource((new SSL())->getSslStreamContext()['stream_context']));
     }
 }

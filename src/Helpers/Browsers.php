8d6ce61121e8cde0605280fcdb73b39b4d834f2a
Tomas Tornevall <thorne@tornevall.net>
2020-02-26 06:47:54 +0100

    Init.

---
+++ src/Helpers/Browsers.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TorneLIB\Helpers;
+
+class Browsers
+{
+    protected $browserList = [
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    ];
+
+    private $mozBrowser = "5.0";
+
+    public function getBrowser()
+    {
+
+    }
+}

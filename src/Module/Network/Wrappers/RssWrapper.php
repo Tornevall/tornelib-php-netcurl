6da5041971cfc45dd0cb44bb2cd2d5c2ce0ccbff
Tomas Tornevall <thorne@tornevall.net>
2020-04-09 08:39:38 +0200

    rsswrapper added

--- src/Module/Network/Wrappers/RssWrapper.php
+++ src/Module/Network/Wrappers/RssWrapper.php
@@ -3,7 +3,18 @@
 namespace TorneLIB\Module\Network\Wrappers;
 
 use TorneLIB\Exception\ExceptionHandler;
+use TorneLIB\Helpers\Version;
+use TorneLIB\Model\Type\authType;
+use TorneLIB\Model\Type\dataType;
+use TorneLIB\Module\Config\WrapperConfig;
+use TorneLIB\Module\Network\Model\requestMethod;
+use TorneLIB\Module\Network\Model\Wrapper;
 
+try {
+    Version::getRequiredVersion();
+} catch (Exception $e) {
+    die($e->getMessage());
+}
 
 /**
  * Class RssWrapper Wrapper to handle RSS feeds.
@@ -12,5 +23,67 @@ use TorneLIB\Exception\ExceptionHandler;
  */
 class RssWrapper implements Wrapper
 {
+    /**
+     * @var WrapperConfig $CONFIG
+     */
+    private $CONFIG;
 
+    /**
+     * RssWrapper constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param WrapperConfig $config
+     * @return RssWrapper
+     */
+    public function setConfig($config)
+    {
+        /** @var WrapperConfig CONFIG */
+        $this->CONFIG = $config;
+
+        return $this;
+    }
+
+    /**
+     * @return WrapperConfig
+     * @since 6.1.0
+     */
+    public function getConfig()
+    {
+        return $this->CONFIG;
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @param int $authType
+     * @return RssWrapper
+     * @since 6.1.0
+     */
+    public function setAuthentication($username, $password, $authType = authType::BASIC)
+    {
+        $this->CONFIG->setAuthentication($username, $password, $authType);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     * @since 6.1.0
+     */
+    public function getAuthentication()
+    {
+        return $this->CONFIG->getAuthentication();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function request($url, $data = [], $method = requestMethod::METHOD_GET, $dataType = dataType::NORMAL)
+    {
+        // TODO: Implement request() method.
+    }
 }
